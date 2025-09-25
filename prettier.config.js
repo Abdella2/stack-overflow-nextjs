198ed7e87932ca4eda61c8f4836ad928908a1984
Abdella Nurahmed <abd.nurahmed@gmail.com>
2025-09-25 20:46:01 +0300

    Setup eslint prettier and tailwind.

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+const prettierConfig = {
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSameLine: true,
+  plugins: ['prettier-plugin-tailwindcss']
+};
+
+export default prettierConfig;
