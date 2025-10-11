fea14055cfc28b3b070b48615108b47a2cd12348
Abdella Nurahmed <abd.nurahmed@gmail.com>
2025-10-11 18:35:20 +0300

    Create tags page

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -1,7 +1,8 @@
 'use server';
 
+import Tag from '@/database/tag.model';
 import { connectToDatabase } from '../mongoose';
-import { GetTopInteractedTagsParams } from './shared.types';
+import { GetAllTagsParams, GetTopInteractedTagsParams } from './shared.types';
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -16,6 +17,19 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
       { _id: '1', name: 'tag 1' },
       { _id: '2', name: 'tag 2' }
     ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    await connectToDatabase();
+
+    const tags = await Tag.find({});
+
+    return { tags };
   } catch (error) {
     console.log(error);
     throw error;
